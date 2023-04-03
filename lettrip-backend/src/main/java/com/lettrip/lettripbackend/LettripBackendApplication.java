57ab0a5546f761a8f303daca12c6347438b96303
subbni <slfkak@naver.com>
2023-04-04 01:27:26 +0900

    SpringBoot 및 dependencies 추가
    - SpringWeb
    - Spring Data JPA
    - MySQL Driver
    - Lombok
    - Validation
    - Spring Security

---
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/LettripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lettrip.lettripbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettripBackendApplication.class, args);
+	}
+
+}
---
+++ lettrip-backend/src/test/java/com/lettrip/lettripbackend/LettripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lettrip.lettripbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
