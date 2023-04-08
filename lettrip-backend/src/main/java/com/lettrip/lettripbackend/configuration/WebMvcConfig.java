9cf8e828bc04e26b60edac29eb0ba40bfb108eb8
subbni <slfkak@naver.com>
2023-04-08 17:05:40 +0900

    #7- WebMvcConfig 설정
    - gradle 에서 SpringSecurity 삭제 (아직 사용하지 않는데 들어가 있으니 외부에서 연결할 수 없고 401 error 발생)
    - WebMvcConfig : 일단 모든 요청 허용

---
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/configuration/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.lettrip.lettripbackend.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","HEAD","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*");
+    }
+}
+
+
