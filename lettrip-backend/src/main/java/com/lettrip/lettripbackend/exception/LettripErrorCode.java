8beff1ffedd03c98e44af955a24e8e8772274657
subbni <slfkak@naver.com>
2023-04-10 18:37:41 +0900

    #7- ErrorCode에 '이메일 인증 코드 불일치' 추가

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/exception/LettripErrorCode.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/exception/LettripErrorCode.java
@@ -7,8 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LettripErrorCode {
     DUPLICATED_EMAIL("이미 해당 Email로 가입된 계정이 존재합니다."),
+    EMAIL_CODE_NOT_MATCH("이메일 인증 코드가 일치하지 않습니다."),
     WRONG_PASSWORD("비밀번호가 일치하지 않습니다."),
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
+
     private final String message;
 }
