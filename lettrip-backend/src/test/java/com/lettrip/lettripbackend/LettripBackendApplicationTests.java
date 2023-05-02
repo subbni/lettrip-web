13099d2910d0885952ef777b93ce730baf83c64a
subbni <slfkak@naver.com>
2023-05-02 17:57:37 +0900

    [fix] ec2 test 실패

--- lettrip-backend/src/test/java/com/lettrip/lettripbackend/LettripBackendApplicationTests.java
+++ lettrip-backend/src/test/java/com/lettrip/lettripbackend/LettripBackendApplicationTests.java
@@ -1,13 +1,9 @@
 package com.lettrip.lettripbackend;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LettripBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
