19c0668e2d04e894bcfd71bb09d3732194d38fe4
subbni <slfkak@naver.com>
2023-05-02 14:08:35 +0900

    errorCode 추가 및 ResourceNotFoundException 추가

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/exception/LettripErrorCode.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/exception/LettripErrorCode.java
@@ -9,8 +9,7 @@ public enum LettripErrorCode {
     DUPLICATED_EMAIL("이미 해당 Email로 가입된 계정이 존재합니다."),
     USER_NOT_FOUND("해당 Email로 가입된 계정이 존재하지 않습니다."),
     EMAIL_CODE_NOT_MATCH("이메일 인증 코드가 일치하지 않습니다."),
-    WRONG_PASSWORD("비밀번호가 일치하지 않습니다."),
-
+    PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다."),
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
 
