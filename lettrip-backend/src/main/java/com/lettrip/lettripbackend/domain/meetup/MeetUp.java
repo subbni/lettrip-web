cf39001dfa37d728a8c2baf06b7f9e2f061a94bd
subbni <slfkak@naver.com>
2023-10-11 15:57:15 +0900

    #60- 친구 매칭 관련 도메인 추가
    - MeetUpPost : 친구 매칭글
    - Poke : 친구 매칭글에 찌르기 (만남 요청)
    - MeeUp : 친구 매칭글로 인해 생성된 만남 약속
    - MeetUpReview : 만남 이후, 혹은 만남 취소 후 작성하는 한줄평 개념

---
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/domain/meetup/MeetUp.java
@@ -0,0 +1,53 @@
+package com.lettrip.lettripbackend.domain.meetup;
+
+import com.lettrip.lettripbackend.constant.MeetUpStatus;
+import com.lettrip.lettripbackend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/*
+- id (unique)
+- meetUpPost id
+- writer (user) id
+- requester (user) id
+- isDateModified (bool)
+- meetUpDate
+- status
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class MeetUp {
+    @Id
+    @Column(name="MEET_UP_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    private MeetUpPost meetUpPost;
+
+    @ManyToOne
+    @JoinColumn(name="WRITE_USER_ID")
+    private User writeUser;
+    @ManyToOne
+    @JoinColumn(name="REQUEST_USER_ID")
+    private User requestUser;
+    private LocalDate meetUpDate;
+
+    @Enumerated(EnumType.STRING)
+    private MeetUpStatus meetUpStatus;
+
+    @Builder
+    public MeetUp( User writeUser, User requestUser,MeetUpPost meetUpPost, LocalDate meetUpDate) {
+        this.writeUser = writeUser;
+        this.requestUser = requestUser;
+        this.meetUpPost= meetUpPost;
+        this.meetUpDate = meetUpDate;
+        this.meetUpStatus = MeetUpStatus.PENDING;
+    }
+
+}
---
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/domain/meetup/MeetUpReview.java
@@ -0,0 +1,49 @@
+package com.lettrip.lettripbackend.domain.meetup;
+
+import com.lettrip.lettripbackend.constant.MeetUpStatus;
+import com.lettrip.lettripbackend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+/*
+- id (unique)
+- MeetUp id
+- Writer user id
+- Object user id
+- content
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class MeetUpReview {
+    @Id
+    @Column(name="MEET_UP_REVIEW_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    private MeetUp meetUp;
+
+    private MeetUpStatus meetUpStatus;
+
+    @ManyToOne
+    @JoinColumn(name="WRITE_USER_ID")
+    private User writeUser;
+
+    @ManyToOne
+    @JoinColumn(name="OBJECT_USER_ID")
+    private User objectUser;
+
+    String content;
+
+    @Builder
+    public MeetUpReview(MeetUp meetUp, MeetUpStatus meetUpStatus, User writeUser, User objectUser, String content) {
+        this.meetUp = meetUp;
+        this.meetUpStatus = meetUpStatus;
+        this.writeUser = writeUser;
+        this.objectUser = objectUser;
+        this.content = content;
+    }
+
+}
