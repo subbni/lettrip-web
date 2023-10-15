543f754bcb895378ddd0fe31aa0ce13d3efbbc34
subbni <slfkak@naver.com>
2023-10-15 16:14:05 +0900

    #60- ChatDto, ChatRoomDto 구현

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/controller/chat/dto/ChatDto.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/controller/chat/dto/ChatDto.java
@@ -1,10 +1,44 @@
 package com.lettrip.lettripbackend.controller.chat.dto;
 
-import lombok.Data;
+import com.lettrip.lettripbackend.mongo.domain.Chat;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 public class ChatDto {
-    private Integer channelId;
-    private Integer writerId;
-    private String message;
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request {
+        private String roomId;
+        private Long senderId;
+        private Long receiverId;
+        private String message;
+        private Boolean isImage;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+        String message;
+        Long senderId;
+        Long receiverId;
+        LocalDateTime createdAt;
+        Boolean isImage;
+
+        public static Response fromEntity(Chat chat) {
+            return Response.builder()
+                    .message(chat.getMessage())
+                    .senderId(chat.getSendUserId())
+                    .receiverId(chat.getReceiveUserId())
+                    .createdAt(chat.getCreatedAt())
+                    .build();
+        }
+    }
 }
