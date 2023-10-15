ae6c8e13af8f02135db8b5519bd6ec38c5db41cb
subbni <slfkak@naver.com>
2023-10-15 16:13:37 +0900

    #60- Chat, ChatRoom 도메인 재정립

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/mongo/domain/ChatRoom.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/mongo/domain/ChatRoom.java
@@ -1,23 +1,51 @@
 package com.lettrip.lettripbackend.mongo.domain;
 
+import com.lettrip.lettripbackend.constant.MeetUpStatus;
+import com.lettrip.lettripbackend.domain.user.User;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document
+@Getter
+@NoArgsConstructor
+@Document(collection = "chat_room")
 public class ChatRoom {
     @Id
     private String id;
     @Field(name="meet_up_post_id")
-    private String meetUpPostId;
+    private long meetUpPostId;
     @Field(name="meet_up_id")
-    private String meetUpId;
+    private long meetUpId;
     @Field(name="write_user_id")
-    private String writeUserId;
+    private long writeUserId;
     @Field(name="request_user_id")
-    private String requestUserId;
+    private long requestUserId;
     @Field(name="last_message")
     private String lastMessage;
     @Field(name="meet_up_status")
-    private String meetUpStatus;
+    @Enumerated(EnumType.STRING)
+    private MeetUpStatus meetUpStatus;
+
+    @Builder
+    public ChatRoom(
+            long meetUpPostId, long meetUpId, long writeUserId,
+            long requestUserId, String lastMessage, MeetUpStatus meetUpStatus
+    ) {
+        this.meetUpPostId = meetUpPostId;
+        this.meetUpId = meetUpId;
+        this.writeUserId = writeUserId;
+        this.requestUserId = requestUserId;
+        this.lastMessage = lastMessage;
+        this.meetUpStatus = meetUpStatus;
+
+    }
+
+    public void updateLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
 }
