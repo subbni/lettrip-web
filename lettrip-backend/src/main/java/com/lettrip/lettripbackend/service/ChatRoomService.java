7f9e68730b0d19886f8cb325e5c5fd511dcd3bdc
subbni <slfkak@naver.com>
2023-10-16 18:58:31 +0900

    #64- User별 채팅방 리스트 조회 API 추가

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/service/ChatRoomService.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/service/ChatRoomService.java
@@ -1,17 +1,27 @@
 package com.lettrip.lettripbackend.service;
 
+import com.lettrip.lettripbackend.controller.chat.dto.ChatDto;
 import com.lettrip.lettripbackend.controller.chat.dto.ChatRoomDto;
+import com.lettrip.lettripbackend.domain.user.User;
 import com.lettrip.lettripbackend.exception.ResourceNotFoundException;
 import com.lettrip.lettripbackend.mongo.domain.ChatRoom;
 import com.lettrip.lettripbackend.mongo.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
 @Service
 public class ChatRoomService {
+    private final UserService userService;
     private final ChatRoomRepository chatRoomRepository;
 
     @Transactional
@@ -27,9 +37,9 @@ public class ChatRoomService {
     }
 
     @Transactional
-    public void updateLastMessage(String roomId, String lastMessage) {
+    public void updateLastMessage(String roomId, String lastMessage, LocalDateTime lastMessageTime) {
         ChatRoom chatRoom = findChatRoomById(roomId);
-        chatRoom.updateLastMessage(lastMessage);
+        chatRoom.updateLastMessageAndTime(lastMessage, lastMessageTime);
         chatRoomRepository.save(chatRoom);
     }
 
@@ -37,4 +47,32 @@ public class ChatRoomService {
         return chatRoomRepository.findById(roomId)
                 .orElseThrow(()-> new ResourceNotFoundException("ChatRoom","chatRoomId",roomId));
     }
+
+    /* 사용자별 채팅 목록 */
+    public Page<ChatRoomDto.Response> getUserChatRoomPage(Long userId, Pageable pageable) {
+        User user = userService.findUserById(userId);
+        Page<ChatRoom> page = chatRoomRepository.findAllByWriteUserIdOrRequestUserId(userId,userId, pageable);
+        return new PageImpl<>(
+                chatRoomToDto(page.getContent(), userId),
+                pageable,
+                page.getTotalElements()
+        );
+    }
+
+    private List<ChatRoomDto.Response> chatRoomToDto(List<ChatRoom> chatRoomList, long userId) {
+        return chatRoomList.stream()
+                .map((chatRoom) -> getChatRoomDto(chatRoom, userId))
+                .collect(Collectors.toList());
+    }
+
+    // 상대 User의 프로필을 찾아 ChatRoomDto의 participant에 넣는다.
+    private ChatRoomDto.Response getChatRoomDto(ChatRoom chatRoom, long userId) {
+        long participantId = (chatRoom.getWriteUserId() != userId) ? chatRoom.getWriteUserId() : chatRoom.getRequestUserId();
+        User participantUser = userService.findUserById(participantId);
+        return ChatRoomDto.Response.fromEntity(chatRoom).setParticipant(participantUser);
+
+    }
+
+
+
 }
