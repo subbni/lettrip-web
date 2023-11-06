f60edb7589b96e089837569cb36e80d35adb5763
subbni <slfkak@naver.com>
2023-11-06 17:32:06 +0900

    [fix] meetUpPost 생성 시 isGpsEnabled 세팅 누락 수정

--- lettrip-backend/src/main/java/com/lettrip/lettripbackend/service/MeetUpPostService.java
+++ lettrip-backend/src/main/java/com/lettrip/lettripbackend/service/MeetUpPostService.java
@@ -53,6 +53,7 @@ public class MeetUpPostService {
                         )
                         .title(request.getTitle())
                         .content(request.getContent())
+                        .isGpsEnabled(request.isGpsEnabled())
                         .place(request.getPlaceId() == null? null : placeService.findById(request.getPlaceId()))
                         .travel(request.getTravelId() == null? null : travelService.findTravelById(request.getTravelId()))
                         .build()
@@ -127,16 +128,6 @@ public class MeetUpPostService {
     public Page<ShowMeetUpPostList.Response> getPokedMeetUpPost(Long userId, Pageable pageable) {
         User user = userService.findUserById(userId);
         List<Poke> pokeList = pokeRepository.findAllByUser(user);
-        List<MeetUpPost> pokedPostList = pokeList.stream()
-                .map((poke)-> {
-                    return meetUpPostRepository.findById(poke.getMeetUpPost().getId())
-                            .orElse(null);
-                }).toList();
-        return new PageImpl<ShowMeetUpPostList.Response>(
-                meetUpPostToDto(pokedPostList),
-                pageable,
-                pokedPostList.size()
-        );
     }
 
     private List<ShowMeetUpPostList.Response> meetUpPostToDto(List<MeetUpPost> meetUpPostList) {
